66553baac1d5f28d4140ba8e1d14e0dc3c3c87c5
thepenguinhead <42331416+thepenguinhead@users.noreply.github.com>
2024-06-21 03:30:36 -0600

    initial

--- app/src/main/java/com/example/a3_01_group5/MainActivity.kt
+++ app/src/main/java/com/example/a3_01_group5/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.example.a3_01_group5
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import androidx.fragment.app.FragmentContainerView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AnswersListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        if (savedInstanceState == null ) {
+
+            findViewById<FragmentContainerView>(R.id.fragment_container)?.let { frameLayout ->
+
+                val questionsFragment = QuestionsFragment()
+
+                supportFragmentManager.beginTransaction()
+                    .add(frameLayout.id, questionsFragment).commit()
+            }
+        }
+    }
+
+    override fun onSelected(questionId: Int) {
+
+        findViewById<FragmentContainerView>(R.id.fragment_container)?.let {frameLayout ->
+
+            val answersFragment = AnswersFragment.newInstance(questionId)
+
+            supportFragmentManager.beginTransaction()
+                .replace(frameLayout.id, answersFragment)
+                .addToBackStack(null)
+                .commit()
         }
     }
 }
